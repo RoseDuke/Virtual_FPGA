7434434286b03c96b02ec42b8b218b88bfbe106a
xtyao <xtyao@bu.edu>
2023-11-18 21:20:48 -0500

    Almost done

--- Virtual_FPGA.py
+++ Virtual_FPGA.py
@@ -2,82 +2,71 @@ from itertools import count
 import re
 import itertools
 import json
-
 from graphviz import Digraph
 
-
 def find_literals(expre):
-    # Find output variable and all input variables
     output, input_list = expre.split('=')
-    input_chars = []
-    for char in input_list:
-        if (char.isalpha() or char.islower()) and char not in input_chars:
-            input_chars.append(char)
-    input_chars.sort()
-
-    return output, input_chars  # Find output variable and all input variables
-
-
-def func_to_dic(expre):
-    # Given a logic equation, generate its truth table
-    val = [0, 1]
-    truth_dic = {}
-
-    simple_expre = expre.split('=')[1]
-
-    literals = re.findall(r'[A-Za-z0-9]+', simple_expre)
-    literals = list(set(literals))
-    literals.sort()
-
-    # print(literals)
-    simple_expre = simple_expre.replace('*', ' and ').replace('+', ' or ')
-    # print(simple_expre)
-    for values in itertools.product(val, repeat=len(literals)):
-        # print(values)
-        context = dict(zip(literals, values))
-        output = eval(simple_expre, {}, context)
-        truth_dic[tuple(values)] = output
-
-    return truth_dic  # Generate Truth table for LUTs
-
+    input_chars = set()
+    for char in re.findall(r'[A-Za-z0-9]+', input_list):
+        input_chars.add(char.rstrip("_'"))
+    return output, sorted(input_chars)
 
 class LUT:
-    def __init__(self, input_vars, output_var, function, dictionary):
+    def __init__(self, input_vars, output_var, logic):
         self.input = input_vars
         self.output = output_var
         self.bits = len(input_vars)
-        self.func = function
-        self.dic = dictionary
+        self.logic = logic
+        self.truth_table = self.logic_to_truth_table(logic)
 
-    def display_dic(self):
-        for key, value in self.dic.items():
-            print(f"{key}:{value}")
+    def logic_to_truth_table(self, expre):
+        """
+        Converts a logic expression to a truth table.
 
+        Args:
+        expre (str): A logic expression in the format "output_var = input_expression".
 
-def connection_config(LUTs_list):
-    # For a LUTs_list, generate connections
-    connection_list = []
-    for lut_a in LUTs_list:
-        for lut_b in LUTs_list:
-            for i in range(len(lut_b.input)):
-                if lut_a.output == lut_b.input[i]:
-                    # Create the connection word for each match
-                    connection_word = [lut_a.func, 'output link to', lut_b.func, 'input', lut_b.input[i]]
-                    # Convert the list to a string
-                    connection_str = " ".join(connection_word)
-                    # Check if the connection string is already in the list
-                    if connection_str not in connection_list:
-                        connection_list.append(connection_str)
+        Returns:
+        dict: A dictionary representing the truth table, mapping input value tuples to output values.
+        """
+        # Define possible binary values for truth table
+        val = [0, 1]
+        truth_dic = {}
 
-    return connection_list
+        # Extract the right-hand side of the equation for evaluation
+        simple_expre = expre.split('=')[1]
+
+        # Collect unique literals while handling inverse notation (e.g., a')
+        literals = set()
+        for literal in re.findall(r'[A-Za-z0-9]+', simple_expre):
+            literals.add(literal.rstrip("_'"))  # Remove any inverse notation
+        literals = sorted(literals)
+
+        # Replace logical operators for Python's eval function
+        simple_expre = simple_expre.replace('*', ' and ').replace('+', ' or ')
+
+        # Generate truth table by evaluating the expression for each combination of input values
+        for values in itertools.product(val, repeat=len(literals)):
+            context = dict(zip(literals, values))
+            # Adjust context for inverses (e.g., a' or a_)
+            for lit in literals:
+                context[lit + "_"] = not context[lit]  # Inverse logic
+                context[lit + "'"] = not context[lit]  # Alternative notation
+            output = eval(simple_expre, {}, context)
+            truth_dic[tuple(values)] = output
+
+        return truth_dic
 
 
 class VirFGPA:
 
-    def __init__(self, sop_dict, total_4_input_LUTs=100, total_6_input_LUTs=100):
+    def __init__(self, sop_dict={}, total_4_input_LUTs=100, total_6_input_LUTs=100):
         self.sop_dict = sop_dict
         self.LUTs_list = []
         self.connection = []
+
+        self.input_vars = set()
+        self.output_vars = set()
 
         self.total_4_input_LUTs = total_4_input_LUTs
         self.total_6_input_LUTs = total_6_input_LUTs
@@ -87,9 +76,15 @@ class VirFGPA:
 
 
     def map_sop_to_LUTs(self):
+        """
+        Maps the given SOP expressions to LUTs.
+        Decomposes complex expressions into smaller sub-expressions and creates LUTs accordingly.
+        """
         intermediate_vars = count(1)
+        generated_vars = set()
 
         for output_var, product_terms in self.sop_dict.items():
+            self.output_vars.add(output_var)
             all_combined_terms = []
 
             for term in product_terms:
@@ -98,20 +93,36 @@ class VirFGPA:
 
             self.create_combined_lut(output_var, all_combined_terms, intermediate_vars)
 
+            # Identifying input variables
+            for term in product_terms:
+                for var in term:
+                    if var not in generated_vars:
+                        self.input_vars.add(var)
+
+            generated_vars.add(output_var)
+
+        # Exclude generated intermediate variables from input variables
+        self.input_vars -= generated_vars
+
     def decompose_term(self, term, intermediate_vars):
+        """
+        Decomposes a single SOP term into smaller subterms that fit into the available LUTs.
+        """
         decomposed_terms = []
         while term:
             sub_term, term = self.get_optimal_subterm(term)
             intermediate_var = f"Int{next(intermediate_vars)}"
             decomposed_terms.append(intermediate_var)
 
-            truth_dic = func_to_dic(f"{intermediate_var} = {' & '.join(sub_term)}")
-            LUT_inst = LUT(sub_term, intermediate_var, f"{intermediate_var} = {' & '.join(sub_term)}", truth_dic)
+            LUT_inst = LUT(sub_term, intermediate_var, f"{intermediate_var} = {' & '.join(sub_term)}")
             self.LUTs_list.append(LUT_inst)
 
         return decomposed_terms
 
     def get_optimal_subterm(self, term):
+        """
+        Chooses the optimal subterm to fit into a LUT based on available LUTs and term length.
+        """
         if len(term) <= 4:
             if self.available_4_inputs_LUTs > 0:
                 # Prioritize 4-input LUT for smaller terms
@@ -136,7 +147,21 @@ class VirFGPA:
 
         return sub_term, term[len(sub_term):]
 
+    def combine_terms(self, terms, intermediate_vars):
+        """
+        Combines multiple terms into a single term using an intermediate variable.
+        """
+        # Combining terms into a single LUT
+        intermediate_var = f"Int{next(intermediate_vars)}"
+        combined_expr = ' & '.join(terms)
+        LUT_inst = LUT(terms, intermediate_var, f"{intermediate_var} = {combined_expr}")
+        self.LUTs_list.append(LUT_inst)
+        return intermediate_var
+
     def create_combined_lut(self, output_var, all_combined_terms, intermediate_vars):
+        """
+        Creates a final LUT for a given output variable by combining all related terms.
+        """
         # Combining all terms into a final LUT
         final_terms = []
         for terms in all_combined_terms:
@@ -148,23 +173,19 @@ class VirFGPA:
 
         self.create_final_lut(output_var, final_terms)
 
-    def combine_terms(self, terms, intermediate_vars):
-        # Combining terms into a single LUT
-        intermediate_var = f"Int{next(intermediate_vars)}"
-        combined_expr = ' & '.join(terms)
-        truth_dic = func_to_dic(f"{intermediate_var} = {combined_expr}")
-        LUT_inst = LUT(terms, intermediate_var, f"{intermediate_var} = {combined_expr}", truth_dic)
-        self.LUTs_list.append(LUT_inst)
-        return intermediate_var
-
     def create_final_lut(self, output_var, final_terms):
+        """
+        Creates the final LUT for an output variable that combines all its terms.
+        """
         # Create the final LUT that ORs the outputs of the combined LUTs
         final_expr = ' | '.join(final_terms)
         if not any(lut.output == output_var for lut in self.LUTs_list):
-            truth_dic = func_to_dic(f"{output_var} = {final_expr}")
-            LUT_inst = LUT(final_terms, output_var, f"{output_var} = {final_expr}", truth_dic)
+            LUT_inst = LUT(final_terms, output_var, f"{output_var} = {final_expr}")
             self.LUTs_list.append(LUT_inst)
     def connect_LUT(self):
+        """
+        connect the LUTs according to the SOP dictionary
+        """
         self.connection = {}
         for i, lut_a in enumerate(self.LUTs_list):
             for j, lut_b in enumerate(self.LUTs_list):
@@ -175,8 +196,11 @@ class VirFGPA:
         return self.connection
 
     def output_bitstream(self):
+        """
+        output the bitstream file, which can be used to restore the FPGA
+        """
         bitstream_data = {
-            "LUTs": [{"id": i, "inputs": lut.input, "output": lut.output, "function": lut.func} for i, lut in
+            "LUTs": [{"id": i, "inputs": lut.input, "output": lut.output, "function": lut.logic} for i, lut in
                      enumerate(self.LUTs_list)],
             "connections": self.connection
         }
@@ -187,21 +211,31 @@ class VirFGPA:
         return 0
 
     def readin_bitstream(self):
+        """
+        read in a bitstream file and restore the FPGA
+        """
         with open('bitstream.json', 'r') as file:
             bitstream_data = json.load(file)
-            self.LUTs_list = [LUT(lut["inputs"], lut["output"], lut["function"], func_to_dic(lut["function"])) for
+            self.LUTs_list = [LUT(lut["inputs"], lut["output"], lut["function"]) for
                               lut in bitstream_data["LUTs"]]
             self.connection = bitstream_data["connections"]
         return self.LUTs_list, self.connection
 
 
     def display_all_info(self, truth_table_enable=0):
+        """
+        print all the information of the FPGA
+        """
+
+        print("Input Variables:", ", ".join(sorted(self.input_vars)))
+        print("Output Variables:", ", ".join(sorted(self.output_vars)))
+
         # Display LUT information
         print("LUT Information:")
         for i, lut in enumerate(self.LUTs_list):
             print(f"LUT {i} (Output: {lut.output}):")
             print(f"  Inputs: {', '.join(lut.input)}")
-            print(f"  Function: {lut.func}")
+            print(f"  Function: {lut.logic}")
             if truth_table_enable == 1:
                 print("  Truth Table:")
                 for key, value in lut.dic.items():
@@ -238,16 +272,38 @@ class VirFGPA:
         print(f"6-input LUTs Usage: {used_6_input_LUTs}/{self.total_6_input_LUTs} ({percent_6_input_usage:.2f}%)")
 
     def draw_diagram(self):
+        """
+        Creates a visual representation of the FPGA layout with nodes for LUTs, input, and output variables.
+        """
         dot = Digraph(comment='The FPGA Diagram')
+
+        # Add nodes for input variables
+        for var in self.input_vars:
+            dot.node(var, var, shape='ellipse', color='lightblue')
 
         # Add nodes for LUTs
         for i, lut in enumerate(self.LUTs_list):
-            dot.node(str(i), f'LUT {i}\n{lut.func}')
+            dot.node(str(i), f'LUT {i}\n{lut.logic}', shape='rectangle')
+
+        # Add nodes for output variables
+        for var in self.output_vars:
+            dot.node(var + "_out", var, shape='ellipse', color='lightgreen')
 
         # Add edges for connections
         for start, ends in self.connection.items():
             for end in ends:
                 dot.edge(str(start), str(end))
+
+        # Connect input variables to their LUTs
+        for lut in self.LUTs_list:
+            for input_var in lut.input:
+                if input_var in self.input_vars:
+                    dot.edge(input_var, str(self.LUTs_list.index(lut)))
+
+        # Connect LUTs to output variables
+        for lut in self.LUTs_list:
+            if lut.output in self.output_vars:
+                dot.edge(str(self.LUTs_list.index(lut)), lut.output + "_out")
 
         # Render the diagram to a file (e.g., in PDF format)
         dot.render('fpga_diagram', view=True)
@@ -255,12 +311,13 @@ class VirFGPA:
 
 # Example SOP Dictionary
 sop_dict = {
-    "X": [['a', 'c'], ['a', 'c', 'b'], ['b', 'd']],
+    "X": [['a', 'a_'], ['a', 'c', 'b'], ['b', 'd']],
     "Y": [['X', 'd']],
     "Z": [['X', 'a'], ['X', 'c', 'd']],
     "W": [['X', 'Y', 'Z'], ['X', 'Z', 'a'], ['X', 'Y']]
 }
-Vir_FPGA_instance = VirFGPA(sop_dict)
+
+Vir_FPGA_instance = VirFGPA(sop_dict, 100, 100)
 Vir_FPGA_instance.map_sop_to_LUTs()
 Vir_FPGA_instance.connect_LUT()
 Vir_FPGA_instance.output_bitstream()
@@ -269,8 +326,9 @@ Vir_FPGA_instance.readin_bitstream()
 Vir_FPGA_instance.display_all_info()
 Vir_FPGA_instance.display_LUT_usage()
 
-# # Example SOP Dictionary
 
+
+# Example SOP Dictionary 2
 
 sop_dict = {
     "X": [['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'],
@@ -279,7 +337,7 @@ sop_dict = {
     "Y": [['X', 't'], ['X', 'r']]
 }
 
-Vir_FPGA_instance = VirFGPA(sop_dict)
+Vir_FPGA_instance = VirFGPA(sop_dict, 100, 0)
 Vir_FPGA_instance.map_sop_to_LUTs()
 Vir_FPGA_instance.connect_LUT()
 Vir_FPGA_instance.output_bitstream()
@@ -287,17 +345,19 @@ Vir_FPGA_instance.output_bitstream()
 Vir_FPGA_instance.readin_bitstream()
 Vir_FPGA_instance.display_all_info()
 Vir_FPGA_instance.display_LUT_usage()
+Vir_FPGA_instance.draw_diagram()
 
-
-Vir_FPGA_instance2 = VirFGPA({})
+Vir_FPGA_instance2 = VirFGPA()
 Vir_FPGA_instance2.readin_bitstream()
-Vir_FPGA_instance2.draw_diagram()
+
+
+
 
 '''
-What to do next?
+To do list:
 1. Inter-dependent SOP functions Done
 2. Logic expression decomposition Done
-3. Output bitstream file and restore from a bitstream file   Corrected tons of bugs
+3. Output bitstream file and restore from a bitstream file  Corrected tons of bugs
 4. A resource allocation summary    Done
-5. A visual representation    !!!Diagram
+5. A visual representation    Done
 '''
